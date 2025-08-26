ef0af36984c859a723c9d2365c7126fe49d4ec22
IgnacioLemma <ilemma@alumnos.exa.unicen.edu.ar>
2025-08-26 10:34:52 -0300

    Add dice game classes and refactor Capitulo class
    Added new Java classes for a procedural dice game: Cubilete, Dado, Juego, JuegoPOO, and Jugador. Refactored Capitulo class to use private fields, boolean flag, added constructors, and improved getter/setter methods for better encapsulation and validation.

--- src/tp2/ej1/Capitulo.java
+++ src/tp2/ej1/Capitulo.java
@@ -1,25 +1,33 @@
 public class Capitulo {
-    String titulo;
-    String descripcion;
-    String flag;
-    double calificacion;
+    private String titulo;
+    private String descripcion;
+    private boolean flag;
+    private double calificacion;
 
 
+    //CONSTRUCTORES
     public Capitulo(String titulo){
         this.titulo = titulo;
         this.descripcion = " ";
-        this.flag = "No visto";
+        this.flag = false;
         this.calificacion = -1;
     }
 
-    public Capitulo(String titulo, String descripcion, String flag, 
+    public Capitulo(String titulo, String descripcion, boolean flag, 
                     double calificacion){
         this.titulo = titulo;
         this.descripcion = descripcion;
         this.flag = flag;
         this.calificacion = calificacion;
     }
+    public Capitulo(String titulo, String descripcion){
+            this.titulo = titulo;
+            this.descripcion = descripcion;
+            this.flag = false;
+            this.calificacion = -1;
+    }
 
+    //GETTERS Y SETTERS
     public String getTitulo() {
         return titulo;
     }
@@ -34,16 +42,21 @@ public class Capitulo {
         this.descripcion = descripcion;
     }
 
-    public String getFlag() {
+    public boolean getFlag() {
         return flag;
     }
-    public void setFlag(String flag) {
+    public void setFlag(boolean flag) {
         this.flag = flag;
     }
 
     public double getCalificacion() {
         return calificacion;
     }
+
+
+    // Ingresar la calificación de un episodio. 
+    //Si el valor ingresado como calificació
+    //Ingresar la calificación de un episodio. Si el valor ingresado como calificación
     public void setCalificacion(double calificacion) {
         if(calificacion >= 0 && calificacion <= 5) {
             this.calificacion = calificacion;
@@ -51,4 +64,5 @@ public class Capitulo {
             System.out.println("Syntax Error");
         }
     }
+
 }
